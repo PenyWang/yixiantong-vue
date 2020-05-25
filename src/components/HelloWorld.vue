464789d3572692d8f7606885733ddeafab2c47b9
skyler <379522872@qq.com>
2020-05-25 19:20:21 +0800

    安装依赖

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -87,6 +87,13 @@
 export default {
   name: 'HelloWorld',
   data () {
+    fetch('http://example.com/movies.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      console.log(myJson);
+    });
     return {
       msg: 'Welcome to Your Vue.js App'
     }
@@ -95,19 +102,19 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
 h1, h2 {
   font-weight: normal;
 }
 ul {
   list-style-type: none;
   padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  li {
+    display: inline-block;
+    margin: 0 10px;
+    a {
+      color: #42b983;
+    }
+  }
 }
 </style>
